6c7b3090e366beae6154dfb2b54ee7bc031853e9
jrubio-m <jrubio-m@student.42madrid.com>
2026-06-01 19:18:04 +0200

    01-06-2026: added expired token in api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -43,6 +43,25 @@ api.interceptors.request.use((config) => {
 	return ( config );
 });
 
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error.response && error.response.status === 401)
+		{
+			if (
+				window.location.pathname !== '/login'
+				&& window.location.pathname !== '/register'
+			)
+			{
+				clearTokens();
+				window.location.href = '/login';
+			}
+		}
+
+		return ( Promise.reject(error) );
+	}
+);
+
 export function getApiErrorMessage(error, fallbackMessage)
 {
 	if (error.response && error.response.data && error.response.data.detail)
