9b06ef132cab0acc665129b16f310e93ff4515cd
jrubio-m <jrubio-m@student.42madrid.com>
2026-06-05 11:18:55 +0200

    05-06-2026: clean game room frontend

--- frontend/src/pages/GameRoomPage.jsx
+++ frontend/src/pages/GameRoomPage.jsx
@@ -2,7 +2,6 @@ import { useParams } from 'react-router-dom';
 
 import { GameRoomLayout } from '../components/game-room/GameRoomLayout';
 import { useGameRoom } from '../hooks/gameRoomHooks/useGameRoom';
-import TopBar from '../components/TopBar';
 
 export default function GameRoomPage()
 {
