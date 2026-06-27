033c79f2c4ff2a37477272ca27f2d2562ab0d9f2
DavidY <davidym343@gmail.com>
2026-06-27 15:04:14 +0200

    feat: enabled https support with self-signed ssl certificate

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 set -e
 
-nginx -g 'daemon off;'
+if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
+  echo "Generating self-signed certificate..."
+  mkdir -p /etc/ssl/private /etc/ssl/certs
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/nginx-selfsigned.key \
+    -out /etc/ssl/certs/nginx-selfsigned.crt \
+    -subj "/C=ES/ST=Madrid/L=Madrid/O=42/OU=42/CN=localhost"
+fi
+
+exec nginx -g 'daemon off;'
